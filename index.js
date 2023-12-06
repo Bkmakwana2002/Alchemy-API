f40faaef4e22f275765362a69801f978a2883188
Bkmakwana2002 <2020meb1278@iitpr.ac.in>
2023-12-06 18:54:17 +0530

    added rate-limiter

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const transaction_routes = require('./routes/transactions')
 const token_routes = require('./routes/token')
 const nft_routes = require('./routes/nft')
 const user_routes = require('./routes/user')
+const { rateLimit } = require('express-rate-limit')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 require("dotenv").config({ path: './.env' })
 connectDB()
 
-app.use('/api',transaction_routes)
-app.use('/api',token_routes)
-app.use('/api',nft_routes)
-app.use('/api',user_routes)
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+    standardHeaders: true, 
+    legacyHeaders: false, 
+})
+
+app.use(limiter)
+app.use('/api', transaction_routes)
+app.use('/api', token_routes)
+app.use('/api', nft_routes)
+app.use('/api', user_routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`)
